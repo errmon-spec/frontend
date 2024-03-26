4593ca5d79ba7980062dd8888633e62772cc1ea2
Lucas Damasceno <lucas.damasceno@al.infnet.edu.br>
2024-03-26 03:08:30 +0000

    dynamically update page title

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,15 @@
+import { nextTick } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import ProjectsView from '../views/ProjectsView.vue';
+
+declare module 'vue-router' {
+  interface RouteMeta {
+    title: string;
+  }
+}
+
+const defaultTitle = 'Errmon';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,13 +18,25 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+      meta: {
+        title: defaultTitle,
+      },
     },
     {
       path: '/projects',
       name: 'projects',
       component: ProjectsView,
+      meta: {
+        title: `Projetos | ${defaultTitle}`,
+      },
     },
   ],
+});
+
+router.afterEach((to) => {
+  nextTick(() => {
+    document.title = to.meta.title || defaultTitle;
+  });
 });
 
 export default router;
