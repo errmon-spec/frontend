b76adc5a1aa7c8550f83cf7dd3137982384204db
Lucas Damasceno <lucas.damasceno@al.infnet.edu.br>
2024-03-26 01:48:49 +0000

    production build: compression (brotli, gzip)

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
 import vue from '@vitejs/plugin-vue';
 import { URL, fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
+import { compression } from 'vite-plugin-compression2';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    compression({ algorithm: 'gzip', exclude: [/\.(br)$/, /\.(gz)$/] }),
+    compression({
+      algorithm: 'brotliCompress',
+      exclude: [/\.(br)$/, /\.(gz)$/],
+    }),
+  ],
+  build: {
+    sourcemap: false,
+    target: 'esnext',
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
